40b33f6d2b92bdc5bb907e2e5c71b132aa95fe70
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-06-01 20:53:09 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
+
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovieList(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
 
+    const getMovies = () => {
+
+      axios
+
+        .get('http://localhost:5000/api/movies')
+
+        .then(response => {
+
+          setMovieList(response.data);
+
+        })
+
+        .catch(error => {
+
+          console.error('Server Error', error);
+
+        });
+
+    }
+
+    getMovies();
+    
+  }, []);
+  //vV ToDo Vv
   const addToSavedList = movie => {
+
     setSavedList([...savedList, movie]);
+
   };
 
   return (
+
     <div>
+      
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/"> <MovieList /> </Route>
+      <Route path="/movies/:id"> <Movie /> </Route>
+
     </div>
+
   );
+
 };
 
 export default App;
